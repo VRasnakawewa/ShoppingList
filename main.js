75202c2fa5cea6e01b0d98f1d5efb96b9cf76845
VRasnakawewa <Vihangarasnakawewa1@gmail.com>
2018-07-13 11:21:31 +0530

    add developer tools

--- main.js
+++ main.js
@@ -71,4 +71,24 @@ const mainMenuTemplate = [
             }
         ]
     }
-]
+];
+
+//add developer tools
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
+
